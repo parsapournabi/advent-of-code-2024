827b171e3a054ba790e0bb378481c1f67d955376
Wearily <parsapournabi1382@gmail.com>
2024-12-13 17:48:14 +0330

    added: Day 1 Part 2.

--- day 1/day.py
+++ day 1/day.py
@@ -1,5 +1,6 @@
 import time
 
+# Part 1
 list_left = []
 list_right = []
 result = 0
@@ -10,5 +11,15 @@ with open('input.txt', 'r') as file:
     list_right.sort()
     for left, right in zip(list_left, list_right):
         result += abs(left - right)
+    print('Answer is:', result)
+    print(time.perf_counter() - st)
+
+# Part 2
+list_left = []
+list_right = []
+with open('input.txt', 'r') as file:
+    st = time.perf_counter()
+    tuple(map(lambda x: (list_left.append(int(x.split()[0])), list_right.append(int(x.split()[1]))), file.readlines()))
+    result = sum([num * list_right.count(num) for num in list_left])
     print('Answer is:', result)
     print(time.perf_counter() - st)
