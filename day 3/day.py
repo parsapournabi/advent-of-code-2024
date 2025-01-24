42d02dddacd823acd2b07f95d8caecaaaad826fe
Wearily <parsapournabi1382@gmail.com>
2025-01-24 23:55:59 +0330

    Day 3 part two Done.

--- day 3/day.py
+++ day 3/day.py
@@ -4,9 +4,27 @@ import re
 with open("input.txt", 'r') as file:
     txt = file.read()
     ans = 0
-    methods = re.findall("mul\(\d+,\d+\)", txt)
+    methods = re.findall(r"mul\(\d+,\d+\)", txt)
 
     for method in methods:
         a, b = map(int, (method.strip("mul()").split(",")))
         ans += a * b
+    print(ans)
+
+# Part Two
+with open("input.txt", 'r') as file:
+    txt = file.read()
+    ans = 0
+    methods = re.findall(r"do\(\)|don't\(\)|mul\(\d+,\d+\)", txt)
+    enabled = True
+    for method in methods:
+        if "do()" in method:
+            enabled = True
+            continue
+        elif "don't()" in method:
+            enabled = False
+            continue
+        if enabled:
+            a, b = map(int, method.strip("mul()").split(","))
+            ans += a * b
     print(ans)
