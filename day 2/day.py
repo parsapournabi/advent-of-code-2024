9e433d45b8db01f094976f54d087c603435b9bef
Wearily <parsapournabi1382@gmail.com>
2025-01-24 23:24:17 +0330

    Day 2 part two Done.

--- day 2/day.py
+++ day 2/day.py
@@ -19,3 +19,21 @@ with open('input.txt', 'r') as file:
     reports = len(list(filter(None, map(lambda x: filter_map(list(map(int, x.split()))), file.readlines()))))
     print(reports)
     print(time.perf_counter() - st)
+
+
+# Part 2
+def safe(report):
+    return all(abs(report[i] - report[i + 1]) in range(1, 4) for i in range(len(report) - 1)) and (
+                all(report[i] > report[i + 1] for i in range(len(report) - 1)) or all(
+            report[i] < report[i + 1] for i in range(len(report) - 1)))
+
+
+with open('input.txt', 'r') as file:
+    st = time.perf_counter()
+    reports = list(map(lambda x: list(map(int, x.split())), file.readlines()))
+    ans = 0
+    for r in reports:
+        if any(safe(r[:i] + r[i + 1:]) for i in range(len(r))):
+            ans += 1
+    print(ans)
+    print(time.perf_counter() - st)
