f4e5e9525059b5e56d8a53a985cfbfe4ec7ca62a
Wearily <parsapournabi1382@gmail.com>
2025-01-28 00:28:09 +0330

    Day 5 Part One Done.

---
+++ day 5/day.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from math import ceil
+
+# Part One
+with open("input.txt", "r") as file:
+    data = file.read().split()
+    pairs = defaultdict(list)
+    for d in data:
+        if "|" in d:
+            k, v = d.split("|")
+            pairs[k].append(v)
+    pages = [d.split(",") for d in data if "," in d]
+
+    ans = 0
+    for i in range(len(pages)):
+        len_page = len(pages[i])
+        al = 0
+        for j in range(len_page):
+            if all(pages[i][k] in pairs[pages[i][j]] for k in range(j + 1, len_page)):
+                al += 1
+        if al >= len_page:
+            ans += int(pages[i][ceil(len(pages[i]) // 2)])
+
+    print(ans)
